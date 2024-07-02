66a5185c72dd492880de18b55b3c13f4c6a35362
rilakkuma3 <xxxibgdrgn.s0818@i.softbank.jp>
2024-07-02 21:58:36 +0900

    querybuilder

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,10 +5,28 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Laravel基礎</title>
+  <style>
+    h1{
+      text-align: center;
+    }
+  </style>
  </head>
  
  <body>
   <h1>投稿一覧</h1>
+  <table>
+         <tr>
+             <th>タイトル</th>
+             <th>本文</th>
+             
+         </tr>
+         @foreach($posts as $post)
+             <tr>
+                 <td>{{ $post->title }}</td>
+                 <td>{{ $post->content }}</td>
+                 
+             </tr>            
+         @endforeach 
 
  </body>
  
